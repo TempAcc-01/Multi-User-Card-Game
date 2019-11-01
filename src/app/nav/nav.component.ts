49c9e262e89ad90573535fdbdcd6dd8544417ce0
unknown <ceilingcat2121@gmail.com>
2019-11-01 11:12:23 -0700

    Project Upload

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../services/game.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor(public Game: GameService) {
+
+    }
+
+  ngOnInit() {
+  }
+
+}
